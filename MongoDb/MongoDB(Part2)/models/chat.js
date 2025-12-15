e207f02c80f7214e9a879dc90923e27567db5abb
Saika Islam <saikaislam0786@gmail.com>
2025-12-15 10:51:15 +0530

    Add files via upload

---
+++ MongoDb/MongoDB(Part2)/models/chat.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const chatSchema = new mongoose.Schema({
+    from: {
+        type: String,
+        required: true
+    },
+    to: {
+        type: String,
+        maxLength: 50
+    },
+    message: {
+        type: String,
+        required: true
+
+    },
+    created_at: {
+        type: Date,
+        required: true
+    }
+});
+
+const Chat = mongoose.model("Chat", chatSchema);
+
+module.exports = Chat;
