777017df53dd4775f3c790456745dd004266d7ef
Saika Islam <saikaislam0786@gmail.com>
2025-12-27 11:07:32 +0530

    Update index.js

--- BACKEND/REST_CLASS/index.js
+++ BACKEND/REST_CLASS/index.js
@@ -33,11 +33,11 @@ let posts = [
 
 app.get("/posts" , (req , res) => {
     res.render("index.ejs",{ posts });
-})
+});
 
 app.get("/posts/new" , (req , res) => {
     res.render("new.ejs");
-})
+});
 
 app.post("/posts" , (req , res) => {
     let { username, content } = req.body;
@@ -65,14 +65,15 @@ app.get("/posts/:id/edit" , (req , res) =>{
     let { id } = req.params;
     let post = posts.find((p) => id === p.id);
     res.render("edit.ejs",{ post });
-})
+});
 
 app.delete("/posts/:id", (req,res) => {
     let { id } = req.params;
     posts = posts.filter((p) => id !== p.id);
     res.redirect("/posts")
-})
+});
 
 app.listen(port , () => {
     console.log("Listening to port 8080");
-})
+});
+
