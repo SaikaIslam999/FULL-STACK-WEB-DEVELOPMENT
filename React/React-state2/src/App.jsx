0870b2f5e27d6e58f4bb38b6aba4defbf02fb596
Saika Islam <saikaislam0786@gmail.com>
2026-03-03 13:43:38 +0530

    Replace LudoBoard with Lottery component

--- React/React-state2/src/App.jsx
+++ React/React-state2/src/App.jsx
@@ -1,10 +1,20 @@
 import './App.css'
+import { sum } from './helper'
+import Lottery from './Lottery'
 import LudoBoard from './LudoBoard'
+import Ticket from './Ticket'
+import TodoList from './TodoList'
+
 
 function App() {
+
+ let winCondition = (ticket) => {
+  return ticket[0] === 0;
+ }
+
  return (
     <>
-    <LudoBoard />
+     <Lottery n={3} winCondition={winCondition}/>
     </>
   )
 }
